842cc869c77c83649d93c4de2bca1f4b10df5e22
Web <sanane@gmail.com>
2019-11-04 18:26:49 +0300

    ilk kayit

---
+++ hesapla.py
@@ -0,0 +1,14 @@
+import datetime as dt
+import math 
+"""
+Karakök bulma uygulaması
+"""
+while True:
+    sayi=input("Bir sayı yazını:")
+    sayi=int(sayi)
+    karesi =sayi**2
+    karekok=sayi**0.5
+    print("Karesi {}".format(karesi))
+    print("Karekökü {}".format(karekok))
+ 
+       
